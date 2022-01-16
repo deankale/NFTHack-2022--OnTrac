3f8edb2af43be658d6efbea9e31e397c9c241d1d
deankale <codekale@gmail.com>
2022-01-15 22:50:47 -0800

    styling on landing page

---
+++ react-landing-page/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
